1944d8adabfb4fcf0e934f34ee530cb10e7a741c
ITO_MZ_GOJGHO\mz_gojgho <gojgho@mz.co.kr>
2023-12-04 14:12:09 +0900

    git first commit

---
+++ src/main/java/com/example/gojghojpa/GojghoJpaApplication.java
@@ -0,0 +1,13 @@
+package com.example.gojghojpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GojghoJpaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GojghoJpaApplication.class, args);
+    }
+
+}
